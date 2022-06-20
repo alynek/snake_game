9a38e281b70fb34c1336cb69ffd4bbfab274cafd
alynek <alyne0x67@gmail.com>
2022-06-20 13:45:38 -0300

    Create move method to snake

--- snake.js
+++ snake.js
@@ -1,19 +1,9 @@
-function setup(){
-    createCanvas(600, 600)
-}
-
-function draw(){
-    background(51)
-    snake.update()
-    snake.show()
-}
-
 const snake = {
     x: 0,
     y: 0,
     width: 10,
     height: 10,
-    xspeed: 1,
+    xspeed: 0,
     yspeed: 0,
     update(){
         this.x += this.xspeed
@@ -22,5 +12,11 @@ const snake = {
 
     show(){
         rect(this.x, this.y, this.width, this.height)
+    },
+
+    move(x, y){
+        this.xspeed = x
+        this.yspeed = y
     }
 }
+
