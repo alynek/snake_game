09e1dfd067590b85e88a793d5e9b5637db8945ec
alynek <alyne0x67@gmail.com>
2022-06-20 13:44:36 -0300

    Refactoring the project

---
+++ game.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas(600, 600)
+}
+
+function draw(){
+    background(51)
+    snake.update()
+    snake.show()
+}
+
+function keyPressed(){
+    switch(keyCode){
+        case UP_ARROW: 
+            snake.move(0, -1)
+            break
+        case DOWN_ARROW:
+            snake.move(0, 1)
+            break
+        case RIGHT_ARROW:
+            snake.move(1, 0)
+            break
+        case LEFT_ARROW:
+            snake.move(-1, 0)
+            break
+    }
+}
