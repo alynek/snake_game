8601f41cfb0d11320e43b8cf953d57f55e99c66b
alynek <alyne0x67@gmail.com>
2022-06-21 09:35:43 -0300

    Add constrain on x and y, for collision handling with screen

--- snake.js
+++ snake.js
@@ -1,17 +1,21 @@
 const snake = {
     x: 0,
     y: 0,
-    width: 10,
-    height: 10,
-    xspeed: 0,
+    xspeed: 1,
     yspeed: 0,
+    scale: 20,
+
     update(){
-        this.x += this.xspeed
-        this.y += this.yspeed
+        this.x += this.xspeed * this.scale
+        this.y += this.yspeed * this.scale
+
+        this.x = constrain(this.x, 0, width - this.scale)
+        this.y = constrain(this.y, 0, height - this.scale)
     },
 
     show(){
-        rect(this.x, this.y, this.width, this.height)
+        fill(255)
+        rect(this.x, this.y, this.scale, this.scale)
     },
 
     move(x, y){
